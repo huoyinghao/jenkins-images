875066783eeba970f8710054f0ddad8dbbb8d70f
huoyinghao <962917302@qq.com>
2023-06-07 11:47:40 +0800

    first build
    Signed-off-by: huoyinghao <962917302@qq.com>

--- build/helper/hack/install.sh
+++ build/helper/hack/install.sh
@@ -16,14 +16,3 @@ echo "installing necessary components"
 wget https://golang.google.cn/dl/go$GO_VERSION.linux-amd64.tar.gz && tar -xvf go$GO_VERSION.linux-amd64.tar.gz && mv go go$GO_VERSION.linux-amd64 && rm -rf go$GO_VERSION.linux-amd64.tar.gz
 wget https://golang.google.cn/dl/go$GO_VERSION.linux-arm64.tar.gz && tar -xvf go$GO_VERSION.linux-arm64.tar.gz && mv go go$GO_VERSION.linux-arm64 && rm -rf go$GO_VERSION.linux-arm64.tar.gz
 wget https://archive.apache.org/dist/maven/maven-3/$MAVEN_VERSION/binaries/apache-maven-$MAVEN_VERSION-bin.tar.gz && tar -xvf apache-maven-$MAVEN_VERSION-bin.tar.gz && rm -rf apache-maven-$MAVEN_VERSION-bin.tar.gz
-# install docker
-wget https://download.docker.com/linux/static/stable/x86_64/docker-24.0.2.tgz && tar -zxvf docker-24.0.2.tgz && mv docker docker-amd64
-wget https://download.docker.com/linux/static/stable/aarch64/docker-24.0.2.tgz && tar -zxvf docker-24.0.2.tgz && mv docker docker-arm64
-# install helm & helm3
-wget https://get.helm.sh/helm-v2.11.0-linux-amd64.tar.gz && tar -zxvf helm-v2.11.0-linux-amd64.tar.gz -C helm2-amd64
-wget https://get.helm.sh/helm-v3.5.0-linux-amd64.tar.gz && tar -zxvf helm-v3.5.0-linux-amd64.tar.gz -C helm3-amd64
-wget https://get.helm.sh/helm-v2.11.0-linux-arm64.tar.gz && tar -zxvf helm-v2.11.0-linux-arm64.tar.gz -C helm2-arm64
-wget https://get.helm.sh/helm-v3.5.0-linux-arm64.tar.gz && tar -zxvf helm-v3.5.0-linux-arm64.tar.gz -C helm3-arm64
-# install kubectl
-wget https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl && chmod +x kubectl && mv kubectl kubectl-amd64
-wget https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/arm64/kubectl && chmod +x kubectl && mv kubectl kubectl-arm64
