90a848e4cd177f860be8ef92fb3731396949e064
huoyinghao <962917302@qq.com>
2023-07-07 11:10:01 +0800

    update podman image
    Signed-off-by: huoyinghao <962917302@qq.com>

--- jenkins-agent/base/hack/install_podman.sh
+++ jenkins-agent/base/hack/install_podman.sh
@@ -13,12 +13,14 @@ if [[ ${ARCH} == 'x86_64' ]]; then
   curl -L -o /etc/yum.repos.d/home:alvistack.repo https://download.opensuse.org/repositories/home:alvistack/CentOS_7/home:alvistack.repo && \
   yum -y install podman fuse-overlayfs && \
   ln -s /usr/bin/podman /usr/bin/docker && \
-  yum -y clean all --enablerepo='*'
+  yum -y clean all --enablerepo='*' && \
+  yum update -y
 elif [[ ${ARCH} == 'aarch64' ]]; then
   curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_7/devel:kubic:libcontainers:stable.repo && \
   yum -y install podman fuse-overlayfs && \
   ln -s /usr/bin/podman /usr/bin/docker && \
-  yum -y clean all --enablerepo='*'
+    yum -y clean all --enablerepo='*' && \
+    yum update -y
 else
   echo "do not support this arch"
   exit 1
