cccf049f0bb95128b0269c91dc5d12e92194c5a9
huoyinghao <962917302@qq.com>
2023-07-07 15:21:15 +0800

    fix install podman fialed error
    Signed-off-by: huoyinghao <962917302@qq.com>

--- jenkins-agent/base/hack/install_podman.sh
+++ jenkins-agent/base/hack/install_podman.sh
@@ -13,14 +13,12 @@ if [[ ${ARCH} == 'x86_64' ]]; then
   curl -L -o /etc/yum.repos.d/home:alvistack.repo https://download.opensuse.org/repositories/home:alvistack/CentOS_7/home:alvistack.repo && \
   yum -y install podman fuse-overlayfs && \
   ln -s /usr/bin/podman /usr/bin/docker && \
-  yum -y clean all --enablerepo='*' && \
-  yum update -y
+  yum -y clean all --enablerepo='*'
 elif [[ ${ARCH} == 'aarch64' ]]; then
   curl -L -o /etc/yum.repos.d/devel:kubic:libcontainers:stable.repo https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/CentOS_7/devel:kubic:libcontainers:stable.repo && \
   yum -y install podman fuse-overlayfs && \
   ln -s /usr/bin/podman /usr/bin/docker && \
-    yum -y clean all --enablerepo='*' && \
-    yum update -y
+  yum -y clean all --enablerepo='*'
 else
   echo "do not support this arch"
   exit 1
